7648178f28ffa3e297941cafc4b75e0f39998344
gloskull <gloskull24@gmail.com>
2026-07-25 19:43:37 +0100

    Add SLO burn rate monitoring

---
+++ src/slo/index.ts
@@ -0,0 +1,13 @@
+export {
+  BurnRateMonitor,
+  SYSTEM_SLO_OBJECTIVES,
+  type BurnRateMonitorOptions,
+  type BurnRateThreshold,
+  type SloAlertSink,
+  type SloEvaluation,
+  type SloLogger,
+  type SloObjective,
+  type SloSignalType,
+  type SloStatus,
+  type SloWindowSample,
+} from './burn_rate_monitor';
